9d28efde171a4ab6adbf9dbca390eef5dc592481
nishad22 <nishad9689@gmail.com>
2023-04-15 14:12:10 +0530

    first commit

---
+++ src/main/java/com/example/springboot/bookcart/BookcartApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.bookcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcartApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/springboot/bookcart/controller/BookController.java
@@ -0,0 +1,5 @@
+package com.example.springboot.bookcart.controller;
+
+public class BookController {
+    int i = 0;
+}
